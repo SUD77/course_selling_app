74e17838be7f74371f3707ef3db661f7ad5e1b04
SUD77 <jaiswalsudhanshu20@gmail.com>
2025-03-09 06:42:16 +0530

    user sign up endpoint

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
+const express = require("express");
 const userRouter = Router();
 
-const express = require("express");
 const { userModel } = require("../db");
 
 const bcrypt = require("bcrypt");
@@ -10,6 +10,8 @@ const jwt = require("jsonwebtoken");
 const JWT_USER_SECRET = "jaiswalsudhanshu20User";
 
 const { z } = require("zod");
+
+userRouter.use(express.json());
 
 function userAuthMiddleware(req, res, next) {
 
@@ -27,10 +29,56 @@ function userAuthMiddleware(req, res, next) {
   }
 }
 
-userRouter.post("/sign-up", function (req, res) {
+userRouter.post("/sign-up",async function (req, res) {
+  
+  //input validation using zod
+  const requiredBody = z.object({
+    email: z.string().min(5).max(100).email(),
+    password: z.string().min(5).max(100),
+    firstName: z.string().min(2),
+    lastName: z.string().min(2)
+  });
+
+
+  const email = req.body.email;
+  const password = req.body.password;
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+
+  //checking if this admin already exists. 
+  const doesUserExist = await userModel.findOne({
+    email: email
+  });
+
+  if (doesUserExist) {
+    return res.status(403).json({
+      message: "User already exists"
+    })
+  }
+
+  //hashing the password
+  try {
+    const hashedPassword = await bcrypt.hash(password, 5);
+    console.log(hashedPassword);
+
+    await userModel.create({
+      email: email,
+      password: hashedPassword,
+      firstName: firstName,
+      lastName: lastName
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      message: "Internal server error"
+    });
+  }
+
   res.json({
-    message: "singup endpoint"
+    message: "User created"
   })
+
+
 });
 
 userRouter.post("/sign-in", function (req, res) {
